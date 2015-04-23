6afa68e12a5d384bb760c898c91b830e313648a9
Matt Woelfel <mwoelfel13@gmail.com>
2015-04-23 17:03:07 -0500

    removed test

--- TimeToLive.js
+++ TimeToLive.js
@@ -1,3 +1,34 @@
 /**
- * Created by braden on 4/23/15.
+ * Created by matt on 4/23/15.
  */
+
+module.exports = function(timeToLiveInput, byteArray) {
+    var self = {};
+
+    if (typeof timeToLiveInput != "undefined") {
+        var timeToLive = timeToLiveInput;
+    }
+    else if (typeof byteArray != "undefined") {
+
+    }
+    else {
+        throw new Error("Invalid parameter was entered!");
+    }
+
+    self.getLengthInBytes = function() {
+
+    }
+
+    self.get = function() {
+        return timeToLive;
+    }
+
+    self.set = function(timeToLive) {
+        timeToLive = timeToLive - 1;
+        return timeToLive;
+    }
+
+    self.getBytes = function() {
+        return
+    }
+}
