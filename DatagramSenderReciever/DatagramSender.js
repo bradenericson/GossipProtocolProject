0b2b3653602dae5a4c451c0fd89c1ca1f5fe4176
braden <braden@jabblabs.com>
2015-04-23 15:25:34 -0500

    added scafollding template to all folders

--- DatagramSenderReciever/DatagramSender.js
+++ DatagramSenderReciever/DatagramSender.js
@@ -1,3 +1,15 @@
 /**
  * Created by braden on 4/23/15.
  */
+
+var exports = {};
+
+exports.sayHelloInEnglish = function() {
+    return "HELLO";
+};
+
+exports.sayHelloInSpanish = function() {
+    return "Hola";
+};
+
+module.exports = exports;
