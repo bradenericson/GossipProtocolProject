f09f84e36f4f0b4d8e93d45d5a05abb2e8c5bc28
Matt Woelfel <mwoelfel13@gmail.com>
2015-04-28 15:48:55 -0500

    fixed TTL getLengthInBytes

--- matt.js
+++ matt.js
@@ -1,3 +1,9 @@
 /**
  * Created by braden on 4/23/15.
  */
+
+var buf = new buffer(id1aaaaaaaaaaaaaid2aaaaaaaaaaaaaTTLXmessageeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
+console.log(buf.getID1());
+console.log(buf.getID2());
+console.log(buf.getTimeToLive());
+console.log(buf.getMessage());
