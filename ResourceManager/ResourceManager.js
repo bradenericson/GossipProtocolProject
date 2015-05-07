3f467484688c89a6dd3c66d38d68e6df83362304
Matt Woelfel <mwoelfel13@gmail.com>
2015-05-07 17:01:58 -0500

    initial resourceManager creation

--- ResourceManager/ResourceManager.js
+++ ResourceManager/ResourceManager.js
@@ -2,14 +2,26 @@
  * Created by braden on 4/23/15.
  */
 
-var exports = {};
+var UDP = require("../DatagramSenderReceiver/UDP/UDPMessage.js");
 
-exports.sayHelloInEnglish = function() {
-    return "HELLO";
+process.on('message', function (m) {
+    var msg = m.getMessage();
+    var tags = msg.split(" ");
+    var badWords = ["the", "and", "a", "an", "on", "of", "from", "that"];
+    var i;
+    var y;
+    for (i = 0; i < tags.length; i++) {
+        for (y = 0; y < badWords.length; y++) {
+            if (tags[i] === badWords[y]) {
+                tags.splice(i);
+            }
+        }
+    }
 };
 
-exports.sayHelloInSpanish = function() {
-    return "Hola";
+function getFromDatabase(callback) {
+  //mongo code
 };
 
-module.exports = exports;
+
+
