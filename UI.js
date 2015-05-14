2863bb1b77994ff1b1db7302b7a19ce987b32fdd
Chad Luangrath <cluangrath1@gmail.com>
2015-05-13 20:59:28 -0500

    Added more UI controls for interacting with resources

--- UI.js
+++ UI.js
@@ -9,6 +9,10 @@ var main = require("./Main.js");
 var childProcess = require("child_process");
 var config = require('./Config.js');
 var Resource = require('./ResourceManager/models/Resource/Resource.js');
+var Spinner = require('cli-spinner').Spinner;
+
+var spinner = new Spinner('processing.. %s');
+spinner.setSpinnerString('|/-\\');
 
 var mongoose = require('mongoose');
 
@@ -30,6 +34,8 @@ help();
 
 process.stdin.on('data', function (text) {
 
+    //spinner.start();
+
     text = text.toLowerCase().trim();
 
     if (text !== 'quit' &&
@@ -37,7 +43,7 @@ process.stdin.on('data', function (text) {
         text.indexOf('search') < 0 &&
         text.indexOf('request') < 0 &&
         text !== 'join' &&
-        text !== 'resources') {
+        text.indexOf('resource') < 0) {
         console.log("You did not enter a valid command. Please enter a valid command.");
         return;
     }
@@ -76,13 +82,39 @@ process.stdin.on('data', function (text) {
             console.log(m);
         });
     }
-    if (text === 'resources') {
-        console.log("in the resources handler");
-        console.log("there are " + resources.length + " records in my database collection");
-        for(var i = 0; i < resources.length; i++) {
-            console.log(resources[i].name);
+    if (text.indexOf("resource") >= 0) {
+        if (text.indexOf("show") >= 0) {
+            console.log("I have " + resources.length + " resources in my collection!");
+            //spinner.start();
+            console.log("\n");
+
+            for(var i = 0; i < resources.length; i++) {
+                var resourceDetailString = resources[i].name + " | " + resources[i].description + " | tags: ";
+
+                for(var j = 0; j < resources[i].tags.length; j++) {
+                    resourceDetailString += resources[i].tags[j] + " ";
+                }
+
+                console.log(resourceDetailString);
+            }
+        }
+        else if (text.indexOf("rename") >= 0) {
+            console.log("renaming resource");
+        }
+        else if (text.indexOf("description") >= 0) {
+            console.log("changing description");
+        }
+        else if (text.indexOf("tags add") >= 0) {
+            console.log("adding tags");
+        }
+        else if (text.indexOf("tags remove") >= 0) {
+            console.log("removing tags");
+        }
+        else {
+            console.log("You did not enter a valid command. Please enter a valid command.");
         }
     }
+
 });
 
 function done() {
@@ -101,7 +133,11 @@ function requestResource(resourceName) {
 function help() {
     console.log("search --searchPhrase | Search for resources using the searchPhrase");
     console.log("request --resourceName | Request a resource by the resource name");
-    console.log("resources | Show my resources");
+    console.log("resource show | Show and manage my resources");
+    console.log("resource rename --resourceName --newResourceName | Rename one of your resources");
+    console.log("resource description --resourceName --newDescription | Change the description of --resourceName");
+    console.log("resource tags add --resourceName --tag1, tag2, tag3, tag4 | Add new tags to --resourceName");
+    console.log("resource tags remove --resourceName --tag1, tag2, tag3, tag4 | Remove tags from --resourceName");
     console.log("join | Join the P2P network");
     console.log("quit | Quit the P2P network");
     console.log("help | See a list of available commands");
